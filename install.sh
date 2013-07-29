6c2ffc3e67f435a36492e8fda963296138a304b5
François Lecomte <francois.lecomte@nereide.fr>
2013-07-29 17:36:36 +0200

    Darwin-based OSes support

--- install.sh
+++ install.sh
@@ -1,6 +1,18 @@
 #!bin/bash
 
-file=/etc/bash_completion.d/adm
+case `uname` in
+	Linux)
+		file=/etc/bash_completion.d/adm
+		;;
+	Darwin)
+		file=/opt/local/etc/bash_completion.d
+		;;
+	*)
+		echo "Your system is not supported yet."
+		echo "If you want to support Amoeba, fill a bug report on https://github.com/fgtl/amoeba/issues with the result of the following command: uname. We'll be delighted to add support for your OS as soon as we hear from you!"
+		echo "Meanwhile, you can install Amoeba by yourself: copy the adm file and paste it in your shell completion folder."
+esac
+
 if [[ -e $file ]]
 then
 	echo "Do you want to remove the existing $file file ? (yes/no)"
